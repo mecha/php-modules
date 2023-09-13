8a7a8f719b241daef424d2c46fa5fa7fc9650552
Miguel Muscat <miguelmuscat93@gmail.com>
2023-09-13 15:50:05 +0200

    test(full): array modules

--- tests/FullTest.php
+++ tests/FullTest.php
@@ -9,13 +9,14 @@ use PHPUnit\Framework\TestCase;
 
 use function Mecha\Modules\scope;
 use function Mecha\Modules\factory;
+use function Mecha\Modules\run;
 use function Mecha\Modules\value;
 use function Mecha\Modules\constant;
 
 class FullTest extends TestCase
 {
     /** @covers App::run */
-    public function test_full(): void
+    public function test_generator_module(): void
     {
         define('USER', 'world');
 
@@ -24,6 +25,34 @@ class FullTest extends TestCase
             yield 'user' => constant('USER');
 
             return factory(function ($greeting, $user) {
+                printf($greeting, $user);
+            }, ['greeting', 'user']);
+        };
+
+        $sModule = scope('foo/', $module());
+
+        $app = new App();
+        $app->addModule($sModule);
+
+        ob_start();
+        $app->run();
+        $actual = ob_get_clean();
+
+        $expected = "hello world\n";
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /** @covers App::run */
+    public function test_array_module(): void
+    {
+        define('USER', 'world');
+
+        $module = function () {
+            yield 'greeting' => value("hello %s\n");
+            yield 'user' => constant('USER');
+
+            yield run(function ($greeting, $user) {
                 printf($greeting, $user);
             }, ['greeting', 'user']);
         };
