65cd823bfc107df2fc60db4fe23ac9e8a4ff16fc
Miguel Muscat <miguelmuscat93@gmail.com>
2023-09-14 10:33:00 +0200

    test(services): add `const` test for `constValue`

--- tests/ConstValueTest.php
+++ tests/ConstValueTest.php
@@ -9,10 +9,12 @@ use PHPUnit\Framework\TestCase;
 
 use function Mecha\Modules\constValue;
 
+const OUTSIDE_CONST = 'a const for testing';
+
 class ConstValueTest extends TestCase
 {
     /** @covers constant */
-    public function test_constant(): void
+    public function test_define(): void
     {
         define('MY_CONSTANT', 'my value');
 
@@ -21,5 +23,15 @@ class ConstValueTest extends TestCase
         $actual = $service($cntr);
 
         $this->assertEquals('my value', $actual);
+    }
+
+    /** @covers constant */
+    public function test_const(): void
+    {
+        $service = constValue(__NAMESPACE__ . '\\OUTSIDE_CONST');
+        $cntr = new TestContainer();
+        $actual = $service($cntr);
+
+        $this->assertEquals(OUTSIDE_CONST, $actual);
     }
 }
